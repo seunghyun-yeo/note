21c8e2ea842e2ceaacca4638a7e1f9b8cb8ece2c
seunghyun-yeo <onyshup@gmail.com>
2022-01-04 23:30:39 -0500

    Today's digging
    check possibility of storing user defined value to stack.

--- asm/test.cpp
+++ asm/test.cpp
@@ -2,39 +2,33 @@
 
 using namespace std;
 uint64_t rrsp, rrbp;
+
 volatile int foo(void) {
   unsigned long val=0x1234567890abcdef;
   __asm__ volatile(
-    "mov %%rsp, %0\n"
-    "mov %%rbp, %1\n"
-    "mov %2, (%%rbp)\n"
+    "mov %0, (%%rbp)\n"
     "nop\n"
-    :"=r"(rrsp), "=r" (rrbp):"r" (val):
+    ::"r" (val):
   );
-    // cout<< "rbp : " << hex << rrbp << dec << endl;
-    // cout<< "rsp : " << hex << rrsp << dec << endl;
   __asm__ volatile(
     "mov (%%rbp), %0\n"
     "nop\n"
     :"=r"(val)::
   );
-  // cout<<"val : " << hex << val << dec << endl;
-  // cout<< "rbp : " << hex << rrbp << dec << endl;
-  rrbp=rrbp+0x20;
+  cout<<"val : " << hex << val << dec << endl;
   __asm__ volatile(
      "mov %0, (%%rbp)\n"
     ::"r" (rrbp):
   );
   return val;
 }
+
 int main(){
   __asm__ volatile(
     "mov %%rsp, %0\n"
     "mov %%rbp, %1\n"
     :"=r"(rrsp), "=r" (rrbp)::
   );
-    cout<< "rbp : " << hex << rrbp << dec << endl;
-    cout<< "rsp : " << hex << rrsp << dec << endl;
     uint64_t a=foo();
     return 0;
 }
